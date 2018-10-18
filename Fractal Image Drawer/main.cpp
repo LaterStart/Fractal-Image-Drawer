346def9701f7b7512eba7b4a3768aca0848c4b8e
mmaderic <mateo.maderic@maidea.hr>
2018-10-18 15:36:44 +0200

    Test drawing image.

--- Fractal Image Drawer/main.cpp
+++ Fractal Image Drawer/main.cpp
@@ -3,7 +3,17 @@
 using namespace std;
 
 int main() {
-	Bitmap bitmap(1920, 1080);
+	const int WIDTH = 1920;
+	const int HEIGHT = 1080;
+
+	Bitmap bitmap(WIDTH, HEIGHT);
+
+	for (int y{}; y < HEIGHT; y++) {
+		for (int x{}; x < WIDTH; x++) {
+			bitmap.SetPixel(x, y, 255, 127, 80);
+		}
+	}
+
 	bitmap.Write("test.bmp");
 
 	return 0;
