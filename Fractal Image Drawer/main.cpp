e1f4a7a044e076329556b35ce8f7af5d456cf729
Mateo Mađerić <mateo.maderic@gmail.com>
2018-10-18 21:40:08 +0200

    First fractal draw

--- Fractal Image Drawer/main.cpp
+++ Fractal Image Drawer/main.cpp
@@ -1,16 +1,25 @@
 #include <iostream>
 #include "Bitmap.h"
+#include "Mangelbrot.h"
 using namespace std;
 
 int main() {
-	const int WIDTH = 1920;
-	const int HEIGHT = 1080;
+	const int WIDTH{ 800 };
+	const int HEIGHT{ 600 };
 
-	Bitmap bitmap(WIDTH, HEIGHT);
+	Bitmap bitmap{ WIDTH, HEIGHT };
 
 	for (int y{}; y < HEIGHT; y++) {
 		for (int x{}; x < WIDTH; x++) {
-			bitmap.SetPixel(x, y, 255, 127, 80);
+			double xFractal((x - WIDTH / 2 - 200)*2.0 / HEIGHT);
+			double yFractal((y - HEIGHT / 2)*2.0 / HEIGHT);
+
+			int iterations = Mangelbrot::GetIterations(xFractal, yFractal);
+
+			uint8_t color(static_cast<uint8_t>((256* (static_cast<double>(iterations) / Mangelbrot::MAX_ITERATIONS))));
+
+			bitmap.SetPixel(x, y, color, 0, 0);
+
 		}
 	}
 
